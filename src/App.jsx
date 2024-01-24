f27d10724db5a53fb7a61e4a0b4a133ad988d67e
w3dd1e <kelly.weddle@gmail.com>
2024-01-24 17:41:04 -0600

    Add profile, title and filter toolbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Posts from './components/Posts';
+import ProfileIcon from './components/ProfileLink';
+import Filter from './components/Filter';
 import './App.css';
+const listOptions = [Recent, Popular];
 
 function App() {
 	return (
 		<>
+			<div className='toolbar'>
+				<ProfileIcon />
+				<h1>GroupBoards</h1>
+				<Filter />
+			</div>
 			<Posts />
-			<Navbar />{' '}
+			<Navbar />
 		</>
 	);
 }
