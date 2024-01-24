d0c9dfe70ba4e13825e5796814c649e47eaf4c1a
w3dd1e <kelly.weddle@gmail.com>
2024-01-24 17:42:53 -0600

    Create profile icon component

---
+++ src/components/ProfileLink.jsx
@@ -0,0 +1,14 @@
+import './styles.css';
+
+const user = { user: 'username', image: '../src/assets/Ducky.jpeg' };
+const userImage = user.image;
+
+function ProfileIcon() {
+	return (
+		<div>
+			<img src={userImage} className='profileIcon' />
+		</div>
+	);
+}
+
+export default ProfileIcon;
