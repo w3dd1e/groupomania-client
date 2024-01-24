95c31a5893b4ebc8305766cbc58c248d33c0296f
w3dd1e <kelly.weddle@gmail.com>
2024-01-24 17:43:28 -0600

    Add profile component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,35 @@
+import Navbar from './Navbar';
+import ProfileIcon from './ProfileLink';
+import Posts from './Posts';
+import '../App.css';
+
+const user = {
+	name: 'Kelly Weddle',
+	location: 'Kansas City',
+	department: 'Sales',
+	about: 'I like movies!',
+};
+
+function Profile() {
+	return (
+		<>
+			<div className='toolbar'>
+				<ProfileIcon />
+				<h1>GroupBoards</h1>
+				<button>Edit</button>
+			</div>
+
+			<div className='profile'>
+				<h2>{user.name}</h2>
+				<p>Department: {user.department}</p>
+				<p>Location: {user.location}</p>
+				<p>About Me: {user.about}</p>
+			</div>
+			<Posts />
+
+			<Navbar />
+		</>
+	);
+}
+
+export default Profile;
