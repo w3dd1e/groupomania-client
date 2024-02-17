f16bbe61b8137cee4ee253dae097be66ba92ef5b
kelly <kelly.weddle@gmail.com>
2024-02-17 16:40:37 -0600

    Add error handling and nested routes to router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./components/Views/App";
+import ErrorPage from "./components/Views/ErrorPage";
+
 import "./index.css";
 
-const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            path: "feed",
+            element: <App />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
