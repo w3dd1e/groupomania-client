c9c99943e2f9eab5764c80068043c0e8ddbe4bf4
kelly <kelly.weddle@gmail.com>
2024-02-17 16:40:21 -0600

    Refactor Navbar component to use NavLink instead of anchor tags -Update link addresses

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,23 +5,25 @@ import {
   FaGear,
   FaMagnifyingGlass,
 } from "react-icons/fa6";
+import { NavLink } from "react-router-dom";
 import "./navbar.css";
 
 function Navbar() {
   const menuLinks = [
-    <FaTableList />,
-    <FaUser />,
-    <FaRegMessage />,
-    <FaMagnifyingGlass />,
-    <FaGear />,
+    { link: "feed", icon: <FaTableList /> },
+    { link: "profile", icon: <FaUser /> },
+    { link: "messages", icon: <FaRegMessage /> },
+    { link: "search", icon: <FaMagnifyingGlass /> },
+    { link: "settings", icon: <FaGear /> },
   ];
+
   return (
     <nav className='nav'>
       <ul className='menu'>
         {menuLinks.map((link) => {
           return (
-            <li key={link}>
-              <a href='#'>{link}</a>
+            <li key={link.link}>
+              <NavLink to={link.link}>{link.icon}</NavLink>
             </li>
           );
         })}
