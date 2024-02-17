471134b78726f671cb6ca547dc5eeb06d57b5ae9
kelly <kelly.weddle@gmail.com>
2024-02-17 17:26:37 -0600

    Add all paths from Nav element

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./components/Views/App";
+import LoginPage from "./components/Views/LoginPage";
+import Posts from "./components/Feed/Posts";
+import Profile from "./components/User/Profile";
+import Inbox from "./components/Inbox/Inbox";
+import Search from "./components/Search/Search";
+import Settings from "./components/User/Settings";
 import ErrorPage from "./components/Views/ErrorPage";
 
 import "./index.css";
@@ -11,13 +17,32 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
+
     children: [
       {
         errorElement: <ErrorPage />,
+
         children: [
+          { index: true, element: <LoginPage /> },
           {
             path: "feed",
-            element: <App />,
+            element: <Posts />,
+          },
+          {
+            path: "profile",
+            element: <Profile />,
+          },
+          {
+            path: "messages",
+            element: <Inbox />,
+          },
+          {
+            path: "search",
+            element: <Search />,
+          },
+          {
+            path: "settings",
+            element: <Settings />,
           },
         ],
       },
