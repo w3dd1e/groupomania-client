075abb2f750ab1a38ee1a979652d1c01c2540798
kelly <kelly.weddle@gmail.com>
2024-02-18 11:02:13 -0600

    Move files for better navigation

---
+++ src/views/Feed.jsx
@@ -0,0 +1,13 @@
+import Posts from "../Posts/Posts";
+import ToolBar from "../ToolBar/ToolBar";
+
+function LoginPage() {
+  return (
+    <div className='feed'>
+      <ToolBar />
+      <Posts />
+    </div>
+  );
+}
+
+export default LoginPage;
