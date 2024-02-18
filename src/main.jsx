e15d24d4be869c84c6980700004192655cfa5519
kelly <kelly.weddle@gmail.com>
2024-02-18 11:24:34 -0600

    Update import paths, add login route

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./components/Views/App";
-import LoginPage from "./components/Views/LoginPage";
-import Posts from "./components/Posts/Posts";
+import App from "./views/App";
+import LoginPage from "./views/LoginPage";
+import Feed from "./views/Feed";
 import Profile from "./components/User/Profile";
 import Inbox from "./components/Inbox/Inbox";
 import Search from "./components/Search/Search";
 import Settings from "./components/User/Settings";
-import ErrorPage from "./components/Views/ErrorPage";
+import ErrorPage from "./views/ErrorPage";
 
 import "./index.css";
 
@@ -25,8 +25,12 @@ const router = createBrowserRouter([
         children: [
           { index: true, element: <LoginPage /> },
           {
+            path: "login",
+            element: <LoginPage />,
+          },
+          {
             path: "feed",
-            element: <Posts />,
+            element: <Feed />,
           },
           {
             path: "profile",
