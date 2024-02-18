033dbef22a9669d0ce99721ccf3ab2156d1bc9e4
kelly <kelly.weddle@gmail.com>
2024-02-18 10:53:55 -0600

    Replace posts with resuable component

--- src/components/Inbox/Inbox.jsx
+++ src/components/Inbox/Inbox.jsx
@@ -1,36 +1,12 @@
 import "../Views/App.css";
-import { FaRegThumbsUp } from "react-icons/fa6";
-import ProfileIcon from "../Titlebar/ProfileLink";
-import Filter from "../Titlebar/Filter";
-
-const posts = [
-  { title: "New Post!", body: "blah blah blah blah blah blah" },
-  { title: "New Post!", body: "blah blah blah blah blah blah" },
-  { title: "New Post!", body: "blah blah blah blah blah blah" },
-];
+import Posts from "../Feed/Posts";
+import Titlebar from "../ToolBar/ToolBar";
 
 function Inbox() {
   return (
     <>
-      <div className='toolbar'>
-        <ProfileIcon />
-        <h1>GroupBoards</h1>
-        <Filter />
-      </div>
-      <div className='board'>
-        <h2>Inbox</h2>
-        {posts.map((post) => {
-          return (
-            <div className='post' key={post}>
-              <div className='postInfo'>
-                <h3>{post.title}</h3>
-                <p>{post.body}</p>
-              </div>
-              <FaRegThumbsUp className='thumbsUp' />
-            </div>
-          );
-        })}
-      </div>
+      <Titlebar />
+      <Posts />
     </>
   );
 }
