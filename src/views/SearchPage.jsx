00520c0a8ef4e49da442c5255669a52376734030
kelly <kelly.weddle@gmail.com>
2024-02-19 16:28:10 -0600

    Create search page

---
+++ src/views/SearchPage.jsx
@@ -0,0 +1,15 @@
+import Search from "../components/Search/Search";
+import Posts from "../components/Posts/Posts";
+import ToolBar from "../components/ToolBar/ToolBar";
+
+function SearchPage() {
+  return (
+    <>
+      <ToolBar />
+      <Search />
+      <Posts />
+    </>
+  );
+}
+
+export default SearchPage;
