4d785edacf2aa0b765a879ac7a88c596d5fe42b9
kelly <kelly.weddle@gmail.com>
2024-02-19 16:31:05 -0600

    Update import paths

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import SignUpPage from "./views/SignUpPage";
 import Feed from "./views/Feed";
 import ProfilePage from "./views/ProfilePage";
 import InboxPage from "./views/InboxPage";
-import Search from "./components/Search/Search";
-import Settings from "./components/User/Settings";
+import SearchPage from "./views/SearchPage";
+import SettingsPage from "./views/SettingsPage";
 import ErrorPage from "./views/ErrorPage";
 
 import "./index.css";
@@ -43,16 +43,27 @@ const router = createBrowserRouter([
             element: <ProfilePage />,
           },
           {
-            path: "messages",
+            path: "inbox",
             element: <InboxPage />,
+            children: [
+              {
+                path: "read",
+                element: <InboxPage />,
+              },
+              {
+                path: "unread",
+                element: <InboxPage />,
+              },
+            ],
           },
+
           {
             path: "search",
             element: <SearchPage />,
           },
           {
             path: "settings",
-            element: <Settings />,
+            element: <SettingsPage />,
           },
         ],
       },
