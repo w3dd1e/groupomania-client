70cdb0bdd7755beac10560510b60c37ff32a1caf
kelly <kelly.weddle@gmail.com>
2024-02-19 16:28:39 -0600

    Update settings componenet to remove outside elements

--- src/components/User/Settings.jsx
+++ src/components/User/Settings.jsx
@@ -1,35 +1,27 @@
-import ProfileIcon from "../ToolBar/ProfileIcon";
 import "./user.css";
 
 function Settings() {
   return (
-    <>
-      <div className='toolbar'>
-        <ProfileIcon />
-        <h1>GroupBoards</h1>
-        <button>Edit</button>
-      </div>
-      <div className='settings'>
-        <h2>Theme</h2>
-        <ul>
-          <li>Dark Mode</li>
-        </ul>
-        <h2>Acessibility</h2>
-        <ul>
-          <li>Text Size</li>
-          <li>Contrast</li>
-        </ul>
+    <div className='settings'>
+      <h2>Theme</h2>
+      <ul>
+        <li>Dark Mode</li>
+      </ul>
+      <h2>Acessibility</h2>
+      <ul>
+        <li>Text Size</li>
+        <li>Contrast</li>
+      </ul>
 
-        <h2>Support</h2>
-        <ul>
-          <li>Content Policy</li>
-          <li>Report Issue</li>
-        </ul>
-        <p className='delete'>
-          <a href='#'>Delete Account</a>
-        </p>
-      </div>
-    </>
+      <h2>Support</h2>
+      <ul>
+        <li>Content Policy</li>
+        <li>Report Issue</li>
+      </ul>
+      <p className='delete'>
+        <a href='#'>Delete Account</a>
+      </p>
+    </div>
   );
 }
 
