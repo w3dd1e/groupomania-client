b8ca97b8d45b65e0009cdd61352b237a69417c6a
kelly <kelly.weddle@gmail.com>
2024-02-19 14:59:57 -0600

    Add InboxPage component

---
+++ src/views/InboxPage.jsx
@@ -0,0 +1,14 @@
+import "./app.css";
+import Posts from "../components/Posts/Posts";
+import ToolBar from "../components/ToolBar/ToolBar";
+
+function Inbox() {
+  return (
+    <>
+      <ToolBar />
+      <Posts />
+    </>
+  );
+}
+
+export default Inbox;
