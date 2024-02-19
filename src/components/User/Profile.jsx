33a6c876a1d61d2e2a57a264c6cb871b8fe1a3b3
kelly <kelly.weddle@gmail.com>
2024-02-19 14:26:01 -0600

    Refactor user profile component for resuability -Remove outside preexisting components

--- src/components/User/Profile.jsx
+++ src/components/User/Profile.jsx
@@ -1,5 +1,3 @@
-import ProfileIcon from "../ToolBar/ProfileLink";
-import Posts from "../Posts/Posts";
 import "./user.css";
 
 const user = {
@@ -7,25 +5,24 @@ const user = {
   location: "Kansas City",
   department: "Sales",
   about: "I like movies!",
+  image: "src/assets/Ducky.jpeg",
 };
 
 function Profile() {
   return (
-    <>
-      <div className='toolbar'>
-        <ProfileIcon />
-        <h1>GroupBoards</h1>
-        <button>Edit</button>
+    <div className='profile'>
+      <img className='profileImage' src={user.image}></img>
+      <div className='about'>
+        <h2 className='fullName'>{user.name}</h2>
+        <p className='aboutInfo'>
+          Username: &emsp;
+          <br />
+          Department: &emsp;{user.department}
+          <br />
+          Location: &emsp;{user.location}
+        </p>
       </div>
-
-      <div className='profile'>
-        <h2>{user.name}</h2>
-        <p>Department: {user.department}</p>
-        <p>Location: {user.location}</p>
-        <p>About Me: {user.about}</p>
-      </div>
-      <Posts />
-    </>
+    </div>
   );
 }
 
