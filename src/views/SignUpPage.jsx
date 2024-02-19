4afd2787a0d3e573f2159a0f122d2ac8c5a09fbe
kelly <kelly.weddle@gmail.com>
2024-02-19 13:52:19 -0600

    Add SignUpPage component

---
+++ src/views/SignUpPage.jsx
@@ -0,0 +1,13 @@
+import SignUp from "../components/Auth/SignUp";
+import Title from "../components/ToolBar/Title";
+
+function SignUpPage() {
+  return (
+    <div className='loginPage'>
+      <Title />
+      <SignUp />
+    </div>
+  );
+}
+
+export default SignUpPage;
