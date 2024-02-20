b7ffdfbf6eb329dab24fc06805c40ef3ebc5f725
kelly <kelly.weddle@gmail.com>
2024-02-19 21:41:27 -0600

    Add React-Query Client

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,17 +1,26 @@
 import Navbar from "../components/Navbar/Navbar";
-
+import {
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Outlet } from "react-router-dom";
-
 import "./app.css";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
-      <div className='content'>
-        <Outlet />
-      </div>
-      <Navbar />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <>
+        <div className='content'>
+          <Outlet />
+        </div>
+        <Navbar />
+      </>
+      <ReactQueryDevtools initialISOpen={true} />
+    </QueryClientProvider>
   );
 }
 
