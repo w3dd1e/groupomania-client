0f1c9848f39efc5b6f667f7215b3d3d616c79a1d
kelly <kelly.weddle@gmail.com>
2024-02-23 14:29:27 -0600

    Remove Reach Query

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,26 +1,16 @@
 import Navbar from "../components/Navbar/Navbar";
-import {
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { Outlet } from "react-router-dom";
 import "./app.css";
 
-const queryClient = new QueryClient();
-
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <>
-        <div className='content'>
-          <Outlet />
-        </div>
-        <Navbar />
-      </>
-      <ReactQueryDevtools initialISOpen={true} />
-    </QueryClientProvider>
+    <>
+      <div className='content'>
+        <Outlet />
+      </div>
+      <Navbar />
+    </>
   );
 }
 
