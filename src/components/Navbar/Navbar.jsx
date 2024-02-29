a6f2aa4814dffcf1665f35a1e6f4eb4b1d4b2cb1
kelly <kelly.weddle@gmail.com>
2024-02-29 16:46:42 -0600

    Update profile link in Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,10 +8,12 @@ import {
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
 
+const userId = sessionStorage.userId;
+
 function Navbar() {
   const menuLinks = [
     { link: "feed", icon: <FaTableList /> },
-    { link: "profile", icon: <FaUser /> },
+    { link: `profile/:${userId}`, icon: <FaUser /> },
     { link: "inbox", icon: <FaRegMessage /> },
     { link: "search", icon: <FaMagnifyingGlass /> },
     { link: "settings", icon: <FaGear /> },
