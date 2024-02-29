106395a4fd80ea54449931183542f7de2cea81b0
kelly <kelly.weddle@gmail.com>
2024-02-29 16:16:15 -0600

    Add loader function to ProfilePage.jsx

--- src/views/ProfilePage.jsx
+++ src/views/ProfilePage.jsx
@@ -1,6 +1,17 @@
-import Profile from "../components/User/Profile";
+import { redirect } from "react-router-dom";
+import Profile, { getProfile } from "../components/User/Profile";
 import Posts from "../components/Posts/Posts";
 import ToolBar from "../components/ToolBar/ToolBar";
+
+export async function loader() {
+  const response = await getProfile();
+
+  if (response.status === 401) {
+    return redirect("/login");
+  } else {
+    return response;
+  }
+}
 
 function ProfilePage() {
   return (
