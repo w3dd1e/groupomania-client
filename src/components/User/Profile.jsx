f6082b1f20fcbc3aeeb58629c770596decbcda6b
kelly <kelly.weddle@gmail.com>
2024-02-29 16:16:56 -0600

    Add getProfile function to fetch user profile data

--- src/components/User/Profile.jsx
+++ src/components/User/Profile.jsx
@@ -8,6 +8,21 @@ const user = {
   image: "src/assets/Ducky.jpeg",
 };
 
+export async function getProfile() {
+  const profileURL = "http://localhost:3000/posts";
+  let getToken = () => {
+    return sessionStorage.getItem("token");
+  };
+  let token = getToken();
+  let response = await fetch(profileURL, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  console.log("Data fetched from database!");
+  return response;
+}
+
 function Profile() {
   return (
     <div className='profile'>
