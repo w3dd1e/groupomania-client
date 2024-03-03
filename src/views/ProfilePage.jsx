ee3befa35d9f698793182e0c1b693f57f11c2055
kelly <kelly.weddle@gmail.com>
2024-03-03 10:30:01 -0600

    Revert "Add getPosts function to loader in ProfilePage.jsx"
    This reverts commit 1d04ef5d285ae82a11590ae71e1787e927a60a0a.

--- src/views/ProfilePage.jsx
+++ src/views/ProfilePage.jsx
@@ -1,17 +1,17 @@
-import { redirect, json } from "react-router-dom";
+import { redirect } from "react-router-dom";
 import Profile, { getProfile } from "../components/User/Profile";
-import Posts, { getPosts } from "../components/Posts/Posts";
+import Posts from "../components/Posts/Posts";
 import ToolBar from "../components/ToolBar/ToolBar";
 
-export const loader = async () => {
-  const [profile, posts] = await Promise.all([getProfile(), getPosts()]);
-  if (profile.status === 401 || posts.status === 401) {
+export async function loader() {
+  const response = await getProfile();
+
+  if (response.status === 401) {
     return redirect("/login");
   } else {
-    //TODO RETURN TWO LOADER RESPONSES
     return response;
   }
-};
+}
 
 function ProfilePage() {
   return (
