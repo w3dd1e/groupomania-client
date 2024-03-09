823cafc628459c5ab3c1fa3e1145e871f60eb0c4
w3dd1e <kelly.weddle@gmail.com>
2024-03-08 21:17:12 -0600

    Refactor file structure and import paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,54 +1,48 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./views/App";
-import LoginPage from "./views/LoginPage";
-import SignUpPage from "./views/SignUpPage";
-import Feed, { loader as feedLoader } from "./views/Feed";
-import ProfilePage, { loader as profileLoader } from "./views/ProfilePage";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './pages/app';
+import ErrorPage from './pages/ErrorPage';
 
-import ErrorPage from "./views/ErrorPage";
-
-import "./index.css";
+import './index.css';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
+	{
+		path: '/',
+		element: <App />,
+		errorElement: <ErrorPage />,
 
-    children: [
-      {
-        errorElement: <ErrorPage />,
+		children: [
+			{
+				errorElement: <ErrorPage />,
+			},
 
-        children: [
-          { index: true, element: <LoginPage /> },
-          {
-            path: "login",
-            element: <LoginPage />,
-          },
-          {
-            path: "signup",
-            element: <SignUpPage />,
-          },
-          {
-            path: "feed",
-            element: <Feed />,
-            loader: feedLoader,
-          },
-          {
-            path: "profile/:userId",
-            element: <ProfilePage />,
-            loader: profileLoader,
-          },
-        ],
-      },
-    ],
-  },
+			/* children: [
+					{ index: true, element: <LoginPage /> },
+					{
+						path: 'login',
+						element: <LoginPage />,
+					},
+					{
+						path: 'signup',
+						element: <SignUpPage />,
+					},
+					{
+						path: 'feed',
+						element: <Feed />,
+						loader: feedLoader,
+					},
+					{
+						path: 'profile/:userId',
+						element: <ProfilePage />,
+						loader: profileLoader,
+					}, */
+		],
+	},
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
