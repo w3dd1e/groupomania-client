9c6f7230fd21ae08111a5768a6877d77e4c35c0a
kelly <kelly.weddle@gmail.com>
2024-03-09 12:54:13 -0600

    Refactor main.jsx -Update paths to redesigned components -Add routing for new post

--- src/main.jsx
+++ src/main.jsx
@@ -1,48 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './pages/app';
-import ErrorPage from './pages/ErrorPage';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./pages/App";
+import ErrorPage from "./pages/ErrorPage";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import Feed from "./components/Feed";
+import Profile from "./components/Profile";
+import NewPost from "./components/NewPost";
 
-import './index.css';
+import "./index.css";
 
 const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />,
-		errorElement: <ErrorPage />,
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
 
-		children: [
-			{
-				errorElement: <ErrorPage />,
-			},
+    children: [
+      {
+        errorElement: <ErrorPage />,
 
-			/* children: [
-					{ index: true, element: <LoginPage /> },
-					{
-						path: 'login',
-						element: <LoginPage />,
-					},
-					{
-						path: 'signup',
-						element: <SignUpPage />,
-					},
-					{
-						path: 'feed',
-						element: <Feed />,
-						loader: feedLoader,
-					},
-					{
-						path: 'profile/:userId',
-						element: <ProfilePage />,
-						loader: profileLoader,
-					}, */
-		],
-	},
+        children: [
+          { index: true, element: <SignIn /> },
+          {
+            path: "login",
+            element: <SignIn />,
+          },
+          {
+            path: "signup",
+            element: <SignUp />,
+          },
+          {
+            path: "feed",
+            element: <Feed />,
+            /*loader: feedLoader,*/
+          },
+          {
+            path: "profile",
+            element: <Profile />,
+            /*loader: profileLoader,*/
+          },
+          {
+            path: "new",
+            element: <NewPost />,
+          },
+        ],
+      },
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
