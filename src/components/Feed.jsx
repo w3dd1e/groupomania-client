369e97f77c889de9329640db39818c7ba41d52a9
kelly <kelly.weddle@gmail.com>
2024-03-09 14:56:10 -0600

    Add more list items for testing

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -77,6 +77,136 @@ export default function Feed() {
               </React.Fragment>
             }
           />
+        </ListItem>{" "}
+        <ListItem alignItems='flex-start' sx={{ pl: 0 }}>
+          <ListItemAvatar>
+            <Avatar alt='Remy Sharp' src='/static/images/avatar/1.jpg' />
+          </ListItemAvatar>
+          <ListItemText
+            primary='Brunch this weekend?'
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: "inline" }}
+                  component='span'
+                  variant='body2'
+                  color='text.primary'
+                >
+                  Ali Connors
+                </Typography>
+                {" — I'll be in your neighborhood doing errands this…"}
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+        <Divider variant='inset' component='li' />
+        <ListItem alignItems='flex-start' sx={{ pl: 0 }}>
+          <ListItemAvatar>
+            <Avatar alt='Travis Howard' src='/static/images/avatar/2.jpg' />
+          </ListItemAvatar>
+          <ListItemText
+            primary='Summer BBQ'
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: "inline" }}
+                  component='span'
+                  variant='body2'
+                  color='text.primary'
+                >
+                  to Scott, Alex, Jennifer
+                </Typography>
+                {" — Wish I could come, but I'm out of town this…"}
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+        <Divider variant='inset' component='li' />
+        <ListItem alignItems='flex-start' sx={{ pl: 0 }}>
+          <ListItemAvatar>
+            <Avatar alt='Cindy Baker' src='/static/images/avatar/3.jpg' />
+          </ListItemAvatar>
+          <ListItemText
+            primary='Oui Oui'
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: "inline" }}
+                  component='span'
+                  variant='body2'
+                  color='text.primary'
+                >
+                  Sandra Adams
+                </Typography>
+                {" — Do you have Paris recommendations? Have you ever…"}
+              </React.Fragment>
+            }
+          />
+        </ListItem>{" "}
+        <ListItem alignItems='flex-start' sx={{ pl: 0 }}>
+          <ListItemAvatar>
+            <Avatar alt='Remy Sharp' src='/static/images/avatar/1.jpg' />
+          </ListItemAvatar>
+          <ListItemText
+            primary='Brunch this weekend?'
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: "inline" }}
+                  component='span'
+                  variant='body2'
+                  color='text.primary'
+                >
+                  Ali Connors
+                </Typography>
+                {" — I'll be in your neighborhood doing errands this…"}
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+        <Divider variant='inset' component='li' />
+        <ListItem alignItems='flex-start' sx={{ pl: 0 }}>
+          <ListItemAvatar>
+            <Avatar alt='Travis Howard' src='/static/images/avatar/2.jpg' />
+          </ListItemAvatar>
+          <ListItemText
+            primary='Summer BBQ'
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: "inline" }}
+                  component='span'
+                  variant='body2'
+                  color='text.primary'
+                >
+                  to Scott, Alex, Jennifer
+                </Typography>
+                {" — Wish I could come, but I'm out of town this…"}
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+        <Divider variant='inset' component='li' />
+        <ListItem alignItems='flex-start' sx={{ pl: 0 }}>
+          <ListItemAvatar>
+            <Avatar alt='Cindy Baker' src='/static/images/avatar/3.jpg' />
+          </ListItemAvatar>
+          <ListItemText
+            primary='Oui Oui'
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: "inline" }}
+                  component='span'
+                  variant='body2'
+                  color='text.primary'
+                >
+                  Sandra Adams
+                </Typography>
+                {" — Do you have Paris recommendations? Have you ever…"}
+              </React.Fragment>
+            }
+          />
         </ListItem>
       </List>{" "}
     </Stack>
