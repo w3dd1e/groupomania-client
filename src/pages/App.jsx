69525f5cf27563efd51a1662125f39f0c7a8c6df
w3dd1e <kelly.weddle@gmail.com>
2024-03-12 23:09:56 -0500

    Move CssBaseline import to top -This fixes the source map error, per Material UI Docs

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,25 +1,25 @@
-import { Outlet } from "react-router-dom";
-import Navigation from "../components/Navigation";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import TitleBar from "../components/TitleBar";
+import CssBaseline from '@mui/material/CssBaseline';
+import { Outlet } from 'react-router-dom';
+import Navigation from '../components/Navigation';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import TitleBar from '../components/TitleBar';
 
 const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-  },
+	palette: {
+		mode: 'dark',
+	},
 });
 
 function App() {
-  return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <TitleBar />
-      <div className='content'>
-        <Outlet />
-      </div>
-      <Navigation />
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<TitleBar />
+			<div className='content'>
+				<Outlet />
+			</div>
+			<Navigation />
+		</ThemeProvider>
+	);
 }
 export default App;
