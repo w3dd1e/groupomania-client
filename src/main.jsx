dde2ca0f05f1721062ce34b8bd38af5a7d622c77
w3dd1e <kelly.weddle@gmail.com>
2024-03-14 00:45:13 -0500

    Update post route path for viewing a single post w/ postId

--- src/main.jsx
+++ src/main.jsx
@@ -1,65 +1,65 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./pages/App";
-import ErrorPage from "./pages/ErrorPage";
-import SignIn from "./components/SignIn";
-import SignUp from "./components/SignUp";
-import Feed from "./components/Feed";
-import Profile, { loader as profileLoader } from "./components/Profile";
-import NewPost from "./components/NewPost";
-import EditProfile from "./components/EditProfile";
-import Delete from "./components/Delete";
-import PostView from "./components/PostView";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './pages/App';
+import ErrorPage from './pages/ErrorPage';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Feed from './components/Feed';
+import Profile, { loader as profileLoader } from './components/Profile';
+import NewPost from './components/NewPost';
+import EditProfile from './components/EditProfile';
+import Delete from './components/Delete';
+import PostView from './components/PostView';
 
-import "./index.css";
+import './index.css';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
+	{
+		path: '/',
+		element: <App />,
+		errorElement: <ErrorPage />,
 
-    children: [
-      {
-        errorElement: <ErrorPage />,
+		children: [
+			{
+				errorElement: <ErrorPage />,
 
-        children: [
-          { index: true, element: <SignIn /> },
-          {
-            path: "login",
-            element: <SignIn />,
-          },
-          {
-            path: "signup",
-            element: <SignUp />,
-          },
-          {
-            path: "feed",
-            element: <Feed />,
-            /*loader: feedLoader,*/
-          },
-          {
-            path: "newPost",
-            element: <NewPost />,
-          },
-          { path: "post", element: <PostView /> },
-          {
-            path: "profile",
-            element: <Profile />,
-            loader: profileLoader,
-          },
+				children: [
+					{ index: true, element: <SignIn /> },
+					{
+						path: 'login',
+						element: <SignIn />,
+					},
+					{
+						path: 'signup',
+						element: <SignUp />,
+					},
+					{
+						path: 'feed',
+						element: <Feed />,
+						/*loader: feedLoader,*/
+					},
+					{
+						path: 'newPost',
+						element: <NewPost />,
+					},
+					{ path: 'post/:postId', element: <PostView /> },
+					{
+						path: 'profile',
+						element: <Profile />,
+						loader: profileLoader,
+					},
 
-          { path: "editProfile", element: <EditProfile /> },
-          { path: "deleteAccount", element: <Delete /> },
-        ],
-      },
-    ],
-  },
+					{ path: 'editProfile', element: <EditProfile /> },
+					{ path: 'deleteAccount', element: <Delete /> },
+				],
+			},
+		],
+	},
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
