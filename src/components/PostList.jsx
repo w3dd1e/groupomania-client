9661b34065c512fc188dfd735a386bbfacf36de8
kelly <kelly.weddle@gmail.com>
2024-03-23 13:26:10 -0500

    Update offset index,  no duplicates returned

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,112 +1,109 @@
-import * as React from 'react';
-import List from '@mui/material/List';
-import InfiniteScroll from 'react-infinite-scroll-component';
-import { Stack } from '@mui/material';
-import { getUserData } from '../helpers/helpers';
-import PostCard from './PostCard';
+import * as React from "react";
+import List from "@mui/material/List";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Stack } from "@mui/material";
+import { getUserData } from "../helpers/helpers";
+import PostCard from "./PostCard";
 
 export default function Feed() {
-	const [items, setItems] = React.useState([]);
-	const [hasMore, setHasMore] = React.useState(true);
-	const [index, setIndex] = React.useState(12);
-	const token = getUserData('token');
+  const [items, setItems] = React.useState([]);
+  const [hasMore, setHasMore] = React.useState(true);
+  const [index, setIndex] = React.useState(12);
+  const token = getUserData("token");
 
-	React.useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const response = await fetch(
-					'http://localhost:3000/posts?offset=0',
-					{
-						headers: {
-							Accept: 'application/json',
-							'Content-Type': 'application/json',
-							Authorization: 'Bearer ' + token,
-						},
-					}
-				);
-				const data = await response.json();
-				await setItems(data);
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/posts?offset=0", {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+          },
+        });
+        const data = await response.json();
+        await setItems(data);
 
-				return data;
-			} catch (err) {
-				console.log(err);
-			}
-		};
-		fetchData();
-	}, [token]);
+        return data;
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [token]);
 
-	const fetchMoreData = async () => {
-		try {
-			let response = await fetch(
-				`http://localhost:3000/posts?offset=${index}`,
-				{
-					headers: {
-						Accept: 'application/json',
-						'Content-Type': 'application/json',
-						Authorization: 'Bearer ' + token,
-					},
-				}
-			);
-			const data = await response.json();
-			await setItems((prevItems) => [...prevItems, ...data]);
+  const fetchMoreData = async () => {
+    try {
+      let response = await fetch(
+        `http://localhost:3000/posts?offset=${index}`,
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+      const data = await response.json();
+      await setItems((prevItems) => [...prevItems, ...data]);
 
-			data.length > 0 ? setHasMore(true) : setHasMore(false);
+      data.length > 0 ? setHasMore(true) : setHasMore(false);
 
-			setIndex((prevIndex) => prevIndex + 10);
-		} catch (err) {
-			console.log(err);
-		}
-	};
+      setIndex((prevIndex) => prevIndex + 12);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	const listPosts = items.map((post) => (
-		<PostCard
-			key={post.post_id}
-			user_id={post.user_id}
-			post_id={post.post_id}
-			headline={post.headline}
-			content={post.content}
-			username={post.user.username}
-			profileImage={post.user.profileImage}
-		/>
-	));
+  const listPosts = items.map((post) => (
+    <PostCard
+      key={post.post_id}
+      user_id={post.user_id}
+      post_id={post.post_id}
+      headline={post.headline}
+      content={post.content}
+      username={post.user.username}
+      profileImage={post.user.profileImage}
+    />
+  ));
 
-	return (
-		<>
-			<h2 className='pageTitle'>Feed</h2>
-			<Stack sx={{ p: 2, pt: 0 }}>
-				<div>
-					<InfiniteScroll
-						dataLength={items.length}
-						next={fetchMoreData}
-						hasMore={hasMore}
-						loader={<p>Loading...</p>}
-						endMessage={<p>You&apos;ve seen all the posts!</p>}
-						refreshFunction={() => this.fetchData()}
-						pullDownToRefresh
-						pullDownToRefreshThreshold={75}
-						pullDownToRefreshContent={
-							<h3 style={{ textAlign: 'center' }}>
-								&#8595; Pull down to refresh
-							</h3>
-						}
-						releaseToRefreshContent={
-							<h3 style={{ textAlign: 'center' }}>
-								&#8593; Release to refresh
-							</h3>
-						}
-					>
-						<List
-							sx={{
-								width: '100%',
-								maxWidth: 360,
-								bgcolor: 'background.paper',
-							}}
-						>
-							{listPosts}
-						</List>
-					</InfiniteScroll>
-				</div>
-			</Stack>
-		</>
-	);
+  return (
+    <>
+      <h2 className='pageTitle'>Feed</h2>
+      <Stack sx={{ p: 2, pt: 0 }}>
+        <div>
+          <InfiniteScroll
+            dataLength={items.length}
+            next={fetchMoreData}
+            hasMore={hasMore}
+            loader={<p>Loading...</p>}
+            endMessage={<p>You&apos;ve seen all the posts!</p>}
+            refreshFunction={() => this.fetchData()}
+            pullDownToRefresh
+            pullDownToRefreshThreshold={75}
+            pullDownToRefreshContent={
+              <h3 style={{ textAlign: "center" }}>
+                &#8595; Pull down to refresh
+              </h3>
+            }
+            releaseToRefreshContent={
+              <h3 style={{ textAlign: "center" }}>
+                &#8593; Release to refresh
+              </h3>
+            }
+          >
+            <List
+              sx={{
+                width: "100%",
+                maxWidth: 360,
+                bgcolor: "background.paper",
+              }}
+            >
+              {listPosts}
+            </List>
+          </InfiniteScroll>
+        </div>
+      </Stack>
+    </>
+  );
 }
