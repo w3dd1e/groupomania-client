7a9030bae17df5c0c5e6c5b215e85f8ec32d3742
kelly <kelly.weddle@gmail.com>
2024-03-23 15:59:14 -0500

    Update Profile and Post layouts to match

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,112 +1,112 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Unstable_Grid2';
-import { Paper } from '@mui/material';
-import CardHeader from '@mui/material/CardHeader';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Card from '@mui/material/Card';
-import { redirect, useLoaderData, Link } from 'react-router-dom';
-import { getUserData } from '../helpers/helpers';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Unstable_Grid2";
+import { Paper } from "@mui/material";
+import CardHeader from "@mui/material/CardHeader";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Card from "@mui/material/Card";
+import { redirect, useLoaderData, Link } from "react-router-dom";
+import { getUserData } from "../helpers/helpers";
 
 //Fetch Post
 async function getPost() {
-	const token = getUserData('token');
-	const path = window.location.pathname;
-	const postId = path.split('/');
-	console.log(path);
+  const token = getUserData("token");
+  const path = window.location.pathname;
+  const postId = path.split("/");
+  console.log(path);
 
-	const postURL = `http://localhost:3000/posts/${postId[2]}`;
+  const postURL = `http://localhost:3000/posts/${postId[2]}`;
 
-	let response = await fetch([postURL], {
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-			Authorization: 'Bearer ' + token,
-		},
-	});
-	if (response.status === 401) {
-		return redirect('/login');
-	}
-	console.log('Data fetched from database!');
-	return response;
+  let response = await fetch([postURL], {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+  });
+  if (response.status === 401) {
+    return redirect("/login");
+  }
+  console.log("Data fetched from database!");
+  return response;
 }
 
 //Loader funciton for Router
 export async function loader() {
-	const res = await getPost();
-	const data = await res.json();
-	return data;
+  const res = await getPost();
+  const data = await res.json();
+  return data;
 }
 
 export default function Post() {
-	const post = useLoaderData();
+  const post = useLoaderData();
 
-	return (
-		<Container component='main' maxWidth='xs' sx={{ p: 2 }}>
-			<h2 className='pageTitle'>Post</h2>
+  return (
+    <Container component='main' maxWidth='xs' sx={{ p: 0 }}>
+      <h2 className='pageTitle'>Post</h2>
 
-			<Grid container justifyContent='center' spacing={3} sx={{ mt: 1 }}>
-				<Grid className='contentBlock'>
-					<Paper elevation={6}>
-						<Card sx={{ maxWidth: 345 }}>
-							<CardHeader
-								avatar={
-									<Avatar
-										alt={post.user.username}
-										src={post.user.profileImage}
-									></Avatar>
-								}
-								title={post.headline}
-								subheader=''
-								titleTypographyProps={{
-									fontSize: 18,
-									fontWeight: 700,
-								}}
-							/>
+      <Stack
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          p: 2,
+        }}
+      >
+        <Paper elevation={1} sx={{ width: 1 }}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardHeader
+              avatar={
+                <Avatar alt={post.username} src={post.profileImage}></Avatar>
+              }
+              title={post.headline}
+              subheader=''
+              titleTypographyProps={{
+                fontSize: 18,
+                fontWeight: 700,
+              }}
+            />
 
-							<CardContent>
-								<Typography
-									variant='body2'
-									color='text.secondary'
-								>
-									{post.content}
-								</Typography>
-							</CardContent>
-							<CardActions
-								sx={{
-									display: 'flex',
-									justifyContent: 'space-between',
-								}}
-							>
-								<Button
-									component={Link}
-									to={`/profile/` + post.user_id}
-									size='small'
-									color='primary'
-								>
-									{post.user.username}
-								</Button>
-								<Typography sx={{ fontSize: 13, pr: '10px' }}>
-									{post.createdAt.split('T')[0]}
-								</Typography>
-							</CardActions>
-						</Card>
-					</Paper>
-				</Grid>
-				<Stack direction='column' flex={1} sx={{ p: 1.5 }}>
-					<Button variant='contained' color='primary' sx={{ my: 2 }}>
-						Edit Post
-					</Button>
-					<Button variant='contained' color='error' sx={{ my: 2 }}>
-						Delete Post
-					</Button>
-				</Stack>
-			</Grid>
-		</Container>
-	);
+            <CardContent>
+              <Typography variant='body2' color='text.secondary'>
+                {post.content}
+              </Typography>
+            </CardContent>
+            <CardActions
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <Button
+                component={Link}
+                to={`/profile/` + post.user_id}
+                size='small'
+                color='primary'
+              >
+                {post.user.username}
+              </Button>
+              <Typography sx={{ fontSize: 13, pr: "10px" }}>
+                {post.createdAt.split("T")[0]}
+              </Typography>
+            </CardActions>
+          </Card>
+        </Paper>
+        <Stack direction='column' flex={1} sx={{ width: 0.5, m: 1 }}>
+          <Button variant='contained' color='primary' sx={{ my: 2 }}>
+            Edit Post
+          </Button>
+          <Button variant='contained' color='error' sx={{ my: 2 }}>
+            Delete Post
+          </Button>
+        </Stack>
+      </Stack>
+    </Container>
+  );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,135 +1,135 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Unstable_Grid2';
-import { Paper } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import { redirect } from 'react-router-dom';
-import { useLoaderData, Link } from 'react-router-dom';
-import { getUserData } from '../helpers/helpers';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Unstable_Grid2";
+import { Paper } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { redirect } from "react-router-dom";
+import { useLoaderData, Link } from "react-router-dom";
+import { getUserData } from "../helpers/helpers";
 
 //Function to get user data from session storage
 
 //Fetch Profile
 async function getProfile() {
-	const token = getUserData('token');
-	const userId = getUserData('userId');
-	const profileURL = `http://localhost:3000/profile/${userId}`;
+  const token = getUserData("token");
+  const userId = getUserData("userId");
+  const profileURL = `http://localhost:3000/profile/${userId}`;
 
-	let response = await fetch(profileURL, {
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-			Authorization: 'Bearer ' + token,
-		},
-	});
-	console.log('Data fetched from database!');
-	return response;
+  let response = await fetch(profileURL, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+  });
+  console.log("Data fetched from database!");
+  return response;
 }
 
 //Loader funciton for Router
 export async function loader() {
-	const response = await getProfile();
+  const response = await getProfile();
 
-	if (response.status === 401) {
-		return redirect('/login');
-	} else {
-		return response;
-	}
+  if (response.status === 401) {
+    return redirect("/login");
+  } else {
+    return response;
+  }
 }
 
 //Create list items for use in the profile
 const ProfileItem = styled(ListItem)(() => ({
-	padding: 0,
-	alignItems: 'flex-start',
+  padding: 0,
+  alignItems: "flex-start",
 }));
 
 export default function Profile() {
-	const user = useLoaderData();
-	return (
-		<Container component='main' maxWidth='xs' sx={{ p: 2 }}>
-			<h2 className='pageTitle'>Profile</h2>
+  const user = useLoaderData();
+  return (
+    <Container component='main' maxWidth='xs' sx={{ p: 0 }}>
+      <h2 className='pageTitle'>Profile</h2>
+      <Stack
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          p: 2,
+        }}
+      >
+        <Paper elevation={1} sx={{ width: 1 }}>
+          <Grid container spacing={2} sx={{ p: 1 }}>
+            <Grid>
+              <Avatar
+                variant='rounded'
+                alt='Ducky'
+                src='../../src/assets/Ducky.jpeg'
+                sx={{
+                  m: 1,
+                  width: 125,
+                  height: 125,
+                  bgcolor: "secondary.main",
+                }}
+              />
+            </Grid>
+            <Grid>
+              <List
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                }}
+              >
+                <ProfileItem alignItems='flex-start'>
+                  <Typography component='h1' variant='h5' fontWeight='bold'>
+                    {user.username}
+                  </Typography>
+                </ProfileItem>
+                <ProfileItem>
+                  <ListItemText primary={user.fullName} />
+                </ProfileItem>
+                <ProfileItem>
+                  <ListItemText primary={user.department} />
+                </ProfileItem>
+                <ProfileItem>
+                  <ListItemText primary={user.location} />
+                </ProfileItem>
+              </List>
+            </Grid>
 
-			<Grid container justifyContent='center' spacing={3} sx={{ mt: 1 }}>
-				<Grid>
-					<Paper elevation={6}>
-						<Grid container spacing={2} sx={{ p: 1 }}>
-							<Grid>
-								<Avatar
-									variant='rounded'
-									alt='Ducky'
-									src='../../src/assets/Ducky.jpeg'
-									sx={{
-										m: 1,
-										width: 125,
-										height: 125,
-										bgcolor: 'secondary.main',
-									}}
-								/>
-							</Grid>
-							<Grid>
-								<List
-									sx={{
-										display: 'flex',
-										flexDirection: 'column',
-										alignItems: 'flex-start',
-									}}
-								>
-									<ProfileItem alignItems='flex-start'>
-										<Typography
-											component='h1'
-											variant='h5'
-											fontWeight='bold'
-										>
-											{user.username}
-										</Typography>
-									</ProfileItem>
-									<ProfileItem>
-										<ListItemText primary={user.name} />
-									</ProfileItem>
-									<ProfileItem>
-										<ListItemText
-											primary={user.department}
-										/>
-									</ProfileItem>
-									<ProfileItem>
-										<ListItemText primary={user.location} />
-									</ProfileItem>
-								</List>
-							</Grid>
+            <Grid sx={{ textAlign: "center", p: 1, m: 1 }}>{user.bio}</Grid>
+          </Grid>
+        </Paper>
 
-							<Grid sx={{ textAlign: 'center', p: 1, m: 1 }}>
-								{user.bio}
-							</Grid>
-						</Grid>
-					</Paper>
-				</Grid>
-				<Stack direction='column' flex={1} sx={{ p: 1.5 }}>
-					<Button
-						component={Link}
-						to='/editProfile'
-						variant='contained'
-						color='primary'
-						sx={{ my: 2 }}
-					>
-						Edit Profile
-					</Button>
-					<Button
-						component={Link}
-						variant='contained'
-						color='error'
-						sx={{ my: 2 }}
-					>
-						Delete Account
-					</Button>
-				</Stack>
-			</Grid>
-		</Container>
-	);
+        <Stack direction='column' flex={1} sx={{ width: 0.5, m: 1 }}>
+          <Button
+            component={Link}
+            to='/editProfile'
+            variant='contained'
+            color='primary'
+            sx={{ my: 2 }}
+            size='small'
+          >
+            Edit Profile
+          </Button>
+          <Button
+            component={Link}
+            variant='contained'
+            color='error'
+            sx={{ my: 2 }}
+            size='small'
+          >
+            Delete Account
+          </Button>
+        </Stack>
+      </Stack>
+    </Container>
+  );
 }
