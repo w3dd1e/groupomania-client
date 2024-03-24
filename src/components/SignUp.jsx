f9d51f7224a3e574de9708014e37a4bd2f395e34
w3dd1e <kelly.weddle@gmail.com>
2024-03-24 00:19:00 -0500

    Move sign up functon to action, & notify on success

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,116 +1,134 @@
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { redirect } from "react-router-dom";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { redirect, Form } from 'react-router-dom';
+
+export const action = async () => {
+	const data = await new FormData(document.querySelector('form'));
+
+	//Convert FormData to JSON
+	const formDataObj = {};
+	data.forEach((value, key) => (formDataObj[key] = value));
+	const json = JSON.stringify(formDataObj);
+
+	try {
+		const response = await fetch('http://localhost:3000/auth/signup', {
+			method: 'POST',
+			body: json,
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+		});
+		if (!response.ok) {
+			let invalidDiv = document.getElementById('invalidUser');
+			invalidDiv.innerHTML = '<p>There was an error.</p>';
+
+			throw new Error(response.status);
+		}
+		if (response.ok) {
+			confirm(
+				'You have successfully signed up! Please log in to continue.'
+			);
+			return redirect('/login');
+		}
+	} catch (error) {
+		console.log('There was a problem with the fetch operation.', error);
+	}
+};
 
 export default function SignUp() {
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    //Convert FormData to JSON
-    const data = new FormData(event.currentTarget);
-    const formDataObj = {};
-    data.forEach((value, key) => (formDataObj[key] = value));
-    const json = JSON.stringify(formDataObj);
+	return (
+		<Container component='main' maxWidth='xs'>
+			<CssBaseline />
+			<Box
+				sx={{
+					marginTop: 8,
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+				}}
+			>
+				<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+					<LockOutlinedIcon />
+				</Avatar>
+				<Typography component='h1' variant='h5'>
+					Sign up
+				</Typography>
+				<Box
+					component={Form}
+					method='post'
+					noValidate
+					sx={{
+						mt: 1,
+						display: 'flex',
+						flexDirection: 'column',
+						alignItems: 'center',
+						width: 1,
+					}}
+				>
+					<Grid container spacing={2}>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								autoComplete='username'
+								name='username'
+								required
+								fullWidth
+								id='username'
+								label='Username'
+								autoFocus
+							/>
+						</Grid>
 
-    try {
-      const response = await fetch("http://localhost:3000/auth/signup", {
-        method: "POST",
-        body: json,
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      });
-      if (!response.ok) {
-        let invalidDiv = document.getElementById("invalidUser");
-        invalidDiv.innerHTML = "<p>There was an error.</p>";
-
-        throw new Error(response.status);
-      }
-      if (response.ok) {
-        return redirect("/login");
-      }
-    } catch (error) {
-      console.log("There was a problem with the fetch operation.", error);
-    }
-  };
-  return (
-    <Container component='main' maxWidth='xs'>
-      <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component='h1' variant='h5'>
-          Sign up
-        </Typography>
-        <Box component='form' noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete='username'
-                name='username'
-                required
-                fullWidth
-                id='username'
-                label='Username'
-                autoFocus
-              />
-            </Grid>
-
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id='email'
-                label='Email Address'
-                name='email'
-                autoComplete='email'
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                name='password'
-                label='Password'
-                type='password'
-                id='password'
-                autoComplete='new-password'
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type='submit'
-            fullWidth
-            variant='contained'
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Sign Up
-          </Button>
-          <Grid container justifyContent='flex-end'>
-            <Grid item>
-              <Link href='login' variant='body2'>
-                Already have an account? Sign in
-              </Link>
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
-    </Container>
-  );
+						<Grid item xs={12}>
+							<TextField
+								required
+								fullWidth
+								id='email'
+								label='Email Address'
+								name='email'
+								autoComplete='email'
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								required
+								fullWidth
+								name='password'
+								label='Password'
+								type='password'
+								id='password'
+								autoComplete='new-password'
+							/>
+						</Grid>
+					</Grid>
+					<Button
+						type='submit'
+						variant='contained'
+						size='small'
+						sx={{ mt: 3, mb: 2, width: 0.5 }}
+					>
+						Sign Up
+					</Button>
+					<Grid
+						container
+						sx={{ display: 'flex', justifyContent: 'center' }}
+					>
+						<Grid item>
+							<Link href='login' variant='body2'>
+								Already have an account? Sign in
+							</Link>
+						</Grid>
+					</Grid>
+				</Box>
+			</Box>
+		</Container>
+	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,88 +1,89 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./pages/App";
-import ErrorPage from "./pages/ErrorPage";
-import SignIn, { action as loginAction } from "./components/SignIn";
-import SignUp from "./components/SignUp";
-import Feed from "./components/PostList";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './pages/App';
+import ErrorPage from './pages/ErrorPage';
+import SignIn, { action as loginAction } from './components/SignIn';
+import SignUp, { action as signUpAction } from './components/SignUp';
+import Feed from './components/PostList';
 import Profile, {
-  loader as profileLoader,
-  action as deleteProfileAction,
-} from "./components/Profile";
-import NewPost, { action as newAction } from "./components/NewPost";
-import EditProfile from "./components/EditProfile";
-import Delete from "./components/Delete";
+	loader as profileLoader,
+	action as deleteProfileAction,
+} from './components/Profile';
+import NewPost, { action as newAction } from './components/NewPost';
+import EditProfile from './components/EditProfile';
+import Delete from './components/Delete';
 import Post, {
-  loader as postLoader,
-  action as deletePostAction,
-} from "./components/Post";
+	loader as postLoader,
+	action as deletePostAction,
+} from './components/Post';
 
-import "./index.css";
+import './index.css';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
+	{
+		path: '/',
+		element: <App />,
+		errorElement: <ErrorPage />,
 
-    children: [
-      {
-        errorElement: <ErrorPage />,
+		children: [
+			{
+				errorElement: <ErrorPage />,
 
-        children: [
-          { index: true, element: <SignIn /> },
-          {
-            path: "login",
-            element: <SignIn />,
-            action: loginAction,
-          },
-          {
-            path: "signup",
-            element: <SignUp />,
-          },
-          {
-            path: "feed",
-            element: <Feed />,
-          },
-          {
-            path: "newPost",
-            element: <NewPost />,
-            action: newAction,
-          },
-          {
-            path: "post/:postId",
-            element: <Post />,
-            loader: postLoader,
-            children: [
-              {
-                path: "delete",
-                action: deletePostAction,
-              },
-            ],
-          },
-          {
-            path: "profile/:userId",
-            element: <Profile />,
-            loader: profileLoader,
-            children: [
-              {
-                path: "delete",
-                action: deleteProfileAction,
-              },
-            ],
-          },
+				children: [
+					{ index: true, element: <SignIn /> },
+					{
+						path: 'login',
+						element: <SignIn />,
+						action: loginAction,
+					},
+					{
+						path: 'signup',
+						element: <SignUp />,
+						action: signUpAction,
+					},
+					{
+						path: 'feed',
+						element: <Feed />,
+					},
+					{
+						path: 'newPost',
+						element: <NewPost />,
+						action: newAction,
+					},
+					{
+						path: 'post/:postId',
+						element: <Post />,
+						loader: postLoader,
+						children: [
+							{
+								path: 'delete',
+								action: deletePostAction,
+							},
+						],
+					},
+					{
+						path: 'profile/:userId',
+						element: <Profile />,
+						loader: profileLoader,
+						children: [
+							{
+								path: 'delete',
+								action: deleteProfileAction,
+							},
+						],
+					},
 
-          { path: "editProfile", element: <EditProfile /> },
-          { path: "deleteAccount", element: <Delete /> },
-        ],
-      },
-    ],
-  },
+					{ path: 'editProfile', element: <EditProfile /> },
+					{ path: 'deleteAccount', element: <Delete /> },
+				],
+			},
+		],
+	},
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
